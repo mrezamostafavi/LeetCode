657267b3a9fe172fc7f9eee5494b808769dca488
Reza <mrezamostafavi98@gmail.com>
2025-03-28 11:51:32 -0600

    update solution

--- Valid Parentheses/solution.py
+++ Valid Parentheses/solution.py
@@ -1,36 +1,37 @@
-s = '[]'
-
-o = '([{'
-c = ')]}'
-z = 0
-open = 0
-idx = []
-
-if s[0] in c or s[-1] in o:
-    print('False')
-else:
-    while open >= 0:
-        for i in s:
-            if i in o:
-                idx.append(o.find(i)) 
-                open += 1
-            else:
-                if open >> 0:
-                    if idx[-1] == c.find(i):
-                        idx.pop(-1)
-                        open -= 1
+class Solution:
+    def isValid(self, s: str) -> bool:
+        o = '([{'
+        c = ')]}'
+        z = 0
+        open = 0
+        idx = []
+        while open >= 0:
+            for i in s:
+                if i in o:
+                    idx.append(o.find(i)) 
+                    open += 1
+                else:
+                    if open >> 0:
+                        if idx[-1] == c.find(i):
+                            idx.pop(-1)
+                            open -= 1
+                        else:
+                            return False
+                            z = 1
+                            break
                     else:
-                        print('False')
+                        return False
                         z = 1
                         break
-                else:
-                    print('False')
-                    z = 1
-                    break
-        if z == 1:
-            break                
-        if z != 1 and open == 0:
-            print('True')
-            break
+            if z == 1:
+                break                
+            if z != 1 and open == 0:
+                return True
+                break
 
-
+# Getting input from the user
+if __name__ == "__main__":
+    input_string = input("Enter the string of brackets: ")
+    solution = Solution()
+    result = solution.isValid(input_string)
+    print(f"The input string is {'valid' if result else 'invalid'}.")
