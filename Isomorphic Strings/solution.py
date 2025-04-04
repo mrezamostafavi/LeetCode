b3d85cb4d5556f53cbfed5cbe62182d115ade858
Reza <mrezamostafavi98@gmail.com>
2025-04-03 22:48:04 -0600

    add solution file

--- Isomorphic Strings/solution.py
+++ Isomorphic Strings/solution.py
@@ -1,19 +1,26 @@
-t = "',.pyfgcrl/=aoeuidhtns-\\;qjkxbmwvz"
-s = "qwertyuiop[]asdfghjkl;'\\zxcvbnm,./"
+def is_isomorphic(s, t):
+    if len(s) != len(t):
+        return False
 
-c = s
-z = t
-if len(s) != len(t) or len(set(s)) != len(set(t)):
-    print('False')
-else:
-    while s:
-        c = c.replace(s[0], t[0].upper())
-        # print(c.lower())
-        s = s.replace(s[0],"")
-        t = t.replace(t[0],"")
-    print(c.lower())
-    print(z)
-    if c.lower() == z:
-        print("True")
-    else:
-        print("False")
+    map_s_to_t = {}
+    map_t_to_s = {}
+
+    for char_s, char_t in zip(s, t):
+        if char_s in map_s_to_t:
+            if map_s_to_t[char_s] != char_t:
+                return False
+        else:
+            map_s_to_t[char_s] = char_t
+
+        if char_t in map_t_to_s:
+            if map_t_to_s[char_t] != char_s:
+                return False
+        else:
+            map_t_to_s[char_t] = char_s
+
+    return True
+
+# Example usage:
+s = "egg"
+t = "add"
+print(is_isomorphic(s, t))  # True
